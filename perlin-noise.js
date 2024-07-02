e4bdd05daa1e541a4d9d864fb87c02fc834c7426
bbricard <baptiste.bricard@etudiant.univ-rennes1.fr>
2024-07-02 19:47:52 +0200

    Petite amélioration de la lisibilité du code

--- perlin-noise.js
+++ perlin-noise.js
@@ -231,26 +231,36 @@ grad.addColorStop(1, "darkblue");
 ctx.fillStyle = grad;
 ctx.fillRect(0,0,canvas.width,canvas.height);
 
+
+function drawPerlin(noise){
+    for(let x=0; x<canvas.width ; x++){
+        for(let y=0; y<canvas.height ; y++){
+            let greaterSide = (canvas.width>canvas.height) ? canvas.width : canvas.height; 
+            var intensity = noise.getPerlin((x+1)/(greaterSide+2),(y+1)/(greaterSide+2)); //Je mets une marge comme ça on est parfait.
+            var high = Math.floor(intensity*256);
+            var line = (high%4==0)? 70 : high
+            ctx.fillStyle = `rgba(0,${line/2},${line},255)`
+            ctx.fillRect(x,y,1,1);
+        }
+    }
+}
+
+
 function applyPerlin(interval){
     const perlin = new PerlinNoise();
     perlin.generateNoise();
-    setInterval(()=>{
-        perlin.rotatePerlin(0.1);
-        for(let x=0; x<canvas.width ; x++){
-            for(let y=0; y<canvas.height ; y++){
-                let greaterSide = (canvas.width>canvas.height) ? canvas.width : canvas.height; 
-                var intensity = perlin.getPerlin((x+1)/(greaterSide+2),(y+1)/(greaterSide+2)); //Je mets une marge comme ça on est parfait.
-                var high = Math.floor(intensity*256);
-                var line = (high%4==0)? 70 : high
-                ctx.fillStyle = `rgba(0,${line/2},${line},255)`
-                ctx.fillRect(x,y,1,1);
-            }
-        }
-    },interval);
+    drawPerlin(perlin);
+    if(interval>1){
+        setInterval(()=>{
+            perlin.rotatePerlin(0.1);
+            drawPerlin(perlin);
+        },interval);
+    }
+    
     
     
 }
 
-applyPerlin(3000);
+applyPerlin(-1);
 
 
